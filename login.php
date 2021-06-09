e74dd3ef823614442466b0d9fda573ea99c9417c
Julien <bocquet-julien@outlook.com>
2021-06-09 16:53:36 +0200

    1er commit

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - PowerofMe</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+
+
+    <nav class="navbar navbar-light bg-light">
+  <div style="text-align: right;">
+    <a class="navbar-brand" href="#">Default</a>
+    <a class="navbar-brand" href="#">Default</a>
+    <a class="navbar-brand" href="#">Default</a>
+  </div>
+</nav>
+
+</head>
+
+<body>
+
+</body>
+
+</html>
